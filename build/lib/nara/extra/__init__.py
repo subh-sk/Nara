c032139b2d13f821a43148cecd5944e2ea14b4ad
Subhash kumar <subhash2004ka@gmail.com>
2024-09-03 14:42:38 +0530

    Delete build/lib/nara directory

--- build/lib/nara/extra/__init__.py
+++
@@ -1,8 +0,0 @@
-from nara.extra.Json.LoadJson import LoadJson
-from nara.extra.Json.Cache import CacheManager
-from nara.extra.Json.Save import JsonList, JsonDict
-from nara.extra.Json.TestingTools import SaveTestResults,LoadTestResults,TimeIt
-from nara.extra.TempMail.tempmail import MailUrl, MailOtp,OnlyMail
-from nara.extra.sql.sql import Sqlmanager
-from nara.extra.Datetime.today_time import async_CurrentDateTime,CurrentDateTime
-from nara.extra.TempMail.tempmail_io import tempmailio
