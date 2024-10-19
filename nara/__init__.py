8fae470931935e889a57fb35c5d9e083dfb8b5dd
Divy0The0Fire <104820663+Divy0The0Fire@users.noreply.github.com>
2024-10-19 12:53:14 +0530

    Update __init__.py

--- nara/__init__.py
+++ nara/__init__.py
@@ -10,3 +10,16 @@ import sys
 from nara.nara.genration.gen_func import CreateFunc
 from nara.nara.genration.gen_template import CreateTemplate
 from nara.nara.genration.initial import init
+
+from nara.nara.llm import (
+    _groq as groq,
+    _openai as openai,
+    _sambanova as sambanova,
+    _openrouter as openrouter,
+    _togrther as together,
+    _cohere as cohere,
+)
+
+from nara.nara.classification import (
+    text_classification
+)
